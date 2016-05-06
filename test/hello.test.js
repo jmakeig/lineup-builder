749702e4f7674fa028730f58c8522e2ba0eca05c
Justin Makeig <jmpublic+github@makeig.com>
2016-05-05 23:29:38 -0700

    Better test template

--- test/hello.test.js
+++ test/hello.test.js
@@ -6,3 +6,10 @@ test('It should equal', (assert) => {
   assert.equal(4, 2+2);
   assert.end();
 });
+
+test('Should throw error', (assert) => {
+  assert.throws(function() { null.f(); }, TypeError);
+  assert.end();
+});
+
+test.skip('This test should be skipped', (assert) => {});
